67575d37a2c02eacc2816f6f719e02895eec5149
Khang Hoang <hoangtrieukhang@gmail.com>
2019-09-29 17:28:51 -0700

    fix: config file resolver

--- src/resolveConfigFile.ts
+++ src/resolveConfigFile.ts
@@ -8,8 +8,8 @@ export async function resolveConfigFile(): Promise<string> {
     "jest",
     document ? document.uri : undefined
   );
-  let filename = config.get<string>("configFileName");
+  let filename = config.get<string>("configFileName") || 'jest.config.js';
   let u = Uri.parse(document.uri);
   let cwd = u.scheme == "file" ? path.dirname(u.fsPath) : workspace.cwd;
-  return await findUp([filename], cwd);
+  return path.join(await findUp([filename], cwd), filename);
 }
